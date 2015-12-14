2d91e094d492fcc81b9c25ee948ba7164ac04fa3
smshen <thanatos1710@gmail.com>
2015-12-14 15:05:22 +0800

    add tpm for tmux

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #! /bin/zsh
 
-PATH='/bin'
+# install develope tools
+brew install zsh tmux
+
+# install tpm for tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 ln -fs "$PWD/vimrc" "$HOME/.vimrc"
 ln -fs "$PWD/tmux.conf" "$HOME/.tmux.conf"
