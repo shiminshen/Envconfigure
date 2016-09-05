d86c2630aaf795aa4d28c553d5f04c8f2a171aed
smshen <thanatos1710@gmail.com>
2016-09-05 13:07:35 +0800

    add os checking and setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,31 @@
 #! /bin/zsh
 
+case `uname` in
+  'Linux' )
+    # install linuxbrew
+    sudo apt-get install curl ruby
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+    PATH="$HOME/.linuxbrew/bin:$PATH"
+    ;;
+  'Mac' )
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    ;;
+esac
+
+brew install zsh tmux git git-extras
+# install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+sudo chsh -s $(which zsh)
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash
+
+# install stable node
+nvm install stable
+
 # install develope tools
-brew install tmux git-extras
+npm install -g instant-markdown-d diff-so-fancy
 
 # install tpm for tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
