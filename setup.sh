e0ca355d72519733bdcf359acfaf7ad0218b194d
smshen <thanatos1710@gmail.com>
2016-09-05 16:14:02 +0800

    fix bug of zsh not found

--- setup.sh
+++ setup.sh
@@ -3,23 +3,24 @@
 case `uname` in
   'Linux' )
     # install linuxbrew
-    sudo apt-get install curl ruby
+    sudo apt-get install zsh curl ruby
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
     PATH="$HOME/.linuxbrew/bin:$PATH"
+    echo 'export PATH="$HOME/.linuxbrew/bin:$PATH"' >>~/.bash_profile
     ;;
   'Mac' )
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew install zsh
     ;;
 esac
 
-brew install zsh tmux git git-extras
-# install ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-sudo chsh -s $(which zsh)
+brew install tmux git git-extras
 
 # install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash
+
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc
+echo '[ -s "$NVM_DIR/nvm.sh"  ] && . "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.bashrc
 
 # install stable node
 nvm install stable
@@ -32,3 +33,12 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 ln -fs "$PWD/vimrc" "$HOME/.vimrc"
 ln -fs "$PWD/tmux.conf" "$HOME/.tmux.conf"
+
+sudo chsh -s $(which zsh)
+
+# install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
+echo '[ -s "$NVM_DIR/nvm.sh"  ] && . "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.zshrc
+echo 'export PATH="$HOME/.linuxbrew/bin:$PATH"' >>~/.zshrc
