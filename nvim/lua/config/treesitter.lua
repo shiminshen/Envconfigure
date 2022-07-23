0f97b9ba454958c362b67e297ce94527ee301b89
Damon Shen <shi-min.shen@yahooinc.com>
2022-07-24 00:57:07 +0800

    fix(nvim): remove code folding

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,17 +1,5 @@
--- vim.opt.foldmethod     = 'expr'
--- vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
----WORKAROUND
-vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {
-  group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
-  callback = function()
-    vim.opt.foldmethod     = 'expr'
-    vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
-  end
-})
----ENDWORKAROUND
-
 require("nvim-treesitter.configs").setup({
-  ensure_installed = {"python", "cpp", "lua", "vim", "javascript"},
+  ensure_installed = {"python", "cpp", "lua", "vim", "javascript", "typescript"},
   ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
