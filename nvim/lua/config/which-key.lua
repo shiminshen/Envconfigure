b1c415b18d3c18afd6a0dd3568fcbfea9f9c2f7f
Damon <damon@deeplearning.ai>
2025-06-09 00:41:02 +0800

    refactor(which-key.lua): update keymap registration to use `add` method and improve readability by using `desc` for descriptions

--- nvim/lua/config/which-key.lua
+++ nvim/lua/config/which-key.lua
@@ -1,34 +1,34 @@
 local wk = require("which-key")
 
-wk.register({
+wk.add({
   -- Group all LSP related keymaps under <leader>l
-  ["<leader>l"] = { name = "LSP" },
+  { "<leader>l", group = "LSP" },
 
   -- Code Action: shows available code actions in a popup (common: <leader>ca)
-  -- ["<leader>ca"] = { "<cmd>Lspsaga code_action<CR>", "Code Action" },
-  ["<leader>qf"] = { "<cmd>Lspsaga code_action<CR>", "Code Action" },
+  -- { "<leader>ca", "<cmd>Lspsaga code_action<CR>", desc = "Code Action" },
+  { "<leader>qf", "<cmd>Lspsaga code_action<CR>", desc = "Code Action" },
 
   -- Show diagnostics at the cursor position (common: <leader>cd)
-  ["<leader>cd"] = { "<cmd>Lspsaga show_cursor_diagnostics ++unfocus<CR>", "Show Cursor Diagnostics" },
+  { "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics ++unfocus<CR>", desc = "Show Cursor Diagnostics" },
 
   -- Show diagnostics for the current line
-  ["<leader>ld"] = { "<cmd>Lspsaga show_line_diagnostics<CR>", "Show Line Diagnostics" },
+  { "<leader>ld", "<cmd>Lspsaga show_line_diagnostics<CR>", desc = "Show Line Diagnostics" },
 
   -- Rename symbol (classic binding: <leader>rn)
-  ["<leader>rn"] = { "<cmd>Lspsaga rename<CR>", "Rename Symbol" },
+  { "<leader>rn", "<cmd>Lspsaga rename<CR>", desc = "Rename Symbol" },
 
   -- Hover documentation on the symbol under the cursor (common: K)
-  ["K"] = { "<cmd>Lspsaga hover_doc<CR>", "Hover Documentation" },
+  { "K", "<cmd>Lspsaga hover_doc<CR>", desc = "Hover Documentation" },
 
   -- -- Go to definition
-  -- ["gd"] = { "<cmd>Lspsaga goto_definition<CR>", "Go to Definition" },
+  -- { "gd", "<cmd>Lspsaga goto_definition<CR>", desc = "Go to Definition" },
   --
   -- -- Peek definition (preview definition without jumping)
-  -- ["gp"] = { "<cmd>Lspsaga peek_definition<CR>", "Peek Definition" },
+  -- { "gp", "<cmd>Lspsaga peek_definition<CR>", desc = "Peek Definition" },
 
   -- Jump to previous diagnostic
-  ["[e"] = { "<cmd>Lspsaga diagnostic_jump_prev<CR>", "Previous Diagnostic" },
+  { "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", desc = "Previous Diagnostic" },
 
   -- Jump to next diagnostic
-  ["]e"] = { "<cmd>Lspsaga diagnostic_jump_next<CR>", "Next Diagnostic" },
+  { "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", desc = "Next Diagnostic" },
 })
